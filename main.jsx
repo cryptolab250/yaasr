a84963c278941abd914cb996f5843a197c6e4410
cryptolab250 <cryptolab250@gmail.com>
2025-04-05 23:29:36 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RifaApp from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RifaApp />
+  </React.StrictMode>
+);
