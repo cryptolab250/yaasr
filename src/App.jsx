420a1f482f5ea86f873b2660a600e31f08d3d984
cryptolab250 <cryptolab250@gmail.com>
2025-04-05 23:27:49 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,2 @@
+// App base de rifas con precio por boleta y estilo moderno oscuro
+{app_code}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RifaApp from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RifaApp />
+  </React.StrictMode>,
+)
